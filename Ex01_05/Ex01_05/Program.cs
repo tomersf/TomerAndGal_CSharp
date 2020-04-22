2ad8696b08738d0d831b40ad6008242463938e39
Tomer <tomer1up@gmail.com>
2020-04-22 17:40:46 +0300

    Start of question 5

---
+++ Ex01_05/Ex01_05/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace B20_Ex01_5
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            InitiateProgram();
+        }
+
+        public static void InitiateProgram()
+        {
+            StringBuilder inputStringFromUser = new StringBuilder();
+            inputStringFromUser.Append(GetStringFromUser());
+            ValidateTheNumberFromUser(inputStringFromUser);
+        }
+        public static string GetStringFromUser()
+        {
+            System.Console.WriteLine("Please enter an 9 digit positive number");
+            return System.Console.ReadLine();
+        }
+
+        public static void ValidateTheNumberFromUser(StringBuilder i_NumberFromUser)
+        {
+            int theNumberFromUser;
+            bool numberIsValid = int.TryParse(i_NumberFromUser.ToString(), out theNumberFromUser);
+            while(!numberIsValid)
+            {
+                System.Console.WriteLine("Bad input! you did not enter a number.");
+                i_NumberFromUser.Remove(0, i_NumberFromUser.Length);
+                i_NumberFromUser.Append(GetStringFromUser());
+                numberIsValid = int.TryParse(i_NumberFromUser.ToString(), out theNumberFromUser);
+            }
+
+            if(theNumberFromUser <= 0)
+            {
+
+
+            }
+
+        }
+    }
+}
